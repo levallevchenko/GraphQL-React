a79468ca7d9ab82c052369a93242fb3f04d9f525
Anna Levchenko <levallevchenko@gmail.com>
2021-08-11 17:05:05 +0300

    refactor customer hook

--- src/app/bus/customer/hooks/useCustomer/index.js
+++ src/app/bus/customer/hooks/useCustomer/index.js
@@ -1,37 +1,25 @@
 // Core
-import { useState } from 'react';
 import { useMutation } from '@apollo/react-hooks';
 import { loader} from 'graphql.macro';
+
+// Hooks
+import { useForm } from '../useForm';
 
 // Mutations
 const mutationCreateAccount = loader('./gql/mutationCreateAccount.graphql');
 
-
 export const useCustomer = () => {
   const [addUser, { data }] = useMutation(mutationCreateAccount);
-  const [values, setValues] =  useState({
-    account: {
-      name: '',
-      username: '',
-      password: ''
-    }
+  const { form, handleChange } =  useForm({
+    name: '',
+    username: '',
+    password: ''
   });
 
-  const handleChange = (evt) => {
-    evt.persist();
-    setValues((prevValues) => ({
-      account: {
-        ...prevValues.account,
-        [evt.target.name]: evt.target.value,
-      }
-    }))
-  }
-
   const save = () => {
-    const { account } = values; 
     addUser({
       variables: {
-        account
+        account: form
       }
     })
   }
