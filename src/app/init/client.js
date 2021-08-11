60a8892002f35f1293430885949b46043294d573
Anna Levchenko <levallevchenko@gmail.com>
2021-08-11 17:03:53 +0300

    change apollo client

--- src/app/init/client.js
+++ src/app/init/client.js
@@ -1,8 +1,17 @@
-import ApolloClient from "apollo-boost";
+// Более продвинутый пакет, позволяет работать с кэшом
+import { ApolloClient } from "apollo-client";
+// ApolloClient требует link, поэтому нужен createHttpLink, позволяет работать с HTTP-запросами 
+import { createHttpLink } from "apollo-link-http";
+import { InMemoryCache} from 'apollo-cache-inmemory';
 
 // GraphQL Server
-const uri = 'https://funded-pet-library.moonhighway.com/';
+const uri = 'https://funded-pet-library.moonhighway.com/'
+const link = createHttpLink({uri});
+
+// Cache initialization
+const cache = new InMemoryCache();
 
 export const client = new ApolloClient({
-  uri
+  cache,
+  link
 });
